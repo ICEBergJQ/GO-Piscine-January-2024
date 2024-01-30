e6c7785be52e5cbfbac0b45af554f8f9b6fca3c5
ICEBergJQ <145236840+ICEBergJQ@users.noreply.github.com>
2024-01-30 10:45:33 +0100

    Update main.go

--- ztail/main.go
+++ ztail/main.go
@@ -3,15 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/01-edu/z01"
 )
-
-func PrintStr(s string) {
-	for i := 0; i < len(s); i++ {
-		z01.PrintRune(rune(s[i]))
-	}
-}
 
 func BasicAtoi(s string) int {
 	var rslt int
@@ -24,56 +16,61 @@ func BasicAtoi(s string) int {
 	return rslt
 }
 
-func checknum(s string) bool {
+func checknum(s string) int {
+	i := 0
 	for _, v := range s {
-		if v < '0' && v > '9' {
-			return false
+		if v >= '0' && v <= '9' {
+			i++
 		}
 	}
-	return true
+	return i
 }
 
 func main() {
 	index := 0
-	arg := os.Args[1:]
-	files := os.Args[3:]
-	if arg[0] == "-c" {
-		if checknum(arg[1]) {
-			index = BasicAtoi(arg[1])
-			if len(arg) > 3 {
+	arg := os.Args
+	if len(arg) < 4 {
+		return
+	}
+	if arg[1] == "-c" {
+		files := os.Args[3:]
+		if checknum(arg[2]) == len(arg[2]) {
+			index = BasicAtoi(arg[2])
+			if len(files) > 1 {
 				for i, v := range files {
 					con, err := os.ReadFile(v)
 					if err != nil {
-						PrintStr(err.Error())
-						PrintStr("\n")
+						fmt.Printf("%s", err.Error())
+						fmt.Printf("\n")
 					} else {
 						if i > 0 {
-							z01.PrintRune('\n')
+							fmt.Printf("\n")
 						}
 						fmt.Printf("==> %s <==\n", v)
 						index = len(con) - index
 						if index < 0 {
-							PrintStr(string(con))
+							fmt.Printf("%s", string(con))
 						} else {
 							for index < len(con) {
-								z01.PrintRune(rune(con[index]))
+								fmt.Printf("%s", string(con[index]))
 								index++
 							}
 						}
 					}
+					index = BasicAtoi(arg[2])
 				}
 			} else {
 				con, err := os.ReadFile(files[0])
 				if err != nil {
-					PrintStr(err.Error())
-					PrintStr("\n")
+					fmt.Printf("%s", err.Error())
+					fmt.Printf("\n")
 				}
 				index = len(con) - index
 				if index < 0 {
-					PrintStr(string(con))
+					fmt.Printf("%s", string(con))
 				} else {
 					for index < len(con) {
-						z01.PrintRune(rune(con[index]))
+						fmt.Printf("%s", string(con[index]))
 						index++
 					}
 				}
