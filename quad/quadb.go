87499989a70fb8ec59a09b9411103e4cbd48e320
ICEBergJQ <145236840+ICEBergJQ@users.noreply.github.com>
2024-02-28 18:52:27 +0100

    Create quadb.go

---
+++ quad/quadb.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadB(x, y int) {
+	for column := 0; column < y; column++ {
+		for line := 0; line < x; line++ {
+			if (line == 0 || line == x-1) && (column == 0 || column == y-1) {
+				if line == 0 && column == 0 {
+					z01.PrintRune('/')
+				} else if line == x-1 && column == 0 {
+					z01.PrintRune('\\')
+				} else if line == 0 && column == y-1 {
+					z01.PrintRune('\\')
+				} else {
+					z01.PrintRune('/')
+				}
+			} else if (line > 0 && line < x-1) && (column > 0 && column < y-1) {
+				z01.PrintRune(' ')
+			} else if (line == 0 || line == x-1) && (column > 0 || column == y-2) {
+				z01.PrintRune('*')
+			} else if column == 0 || column == y-1 {
+				z01.PrintRune('*')
+			}
+			if line == x-1 {
+				z01.PrintRune('\n')
+			}
+		}
+	}
+}
